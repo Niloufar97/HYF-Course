1b0e11123ad6c83b4570895997e97a5727041d09
Niloufar <nilooooofar97@gmail.com>
2023-11-16 10:03:38 +0100

    Arrival time task completed

--- week3/warmup.js
+++ week3/warmup.js
@@ -11,3 +11,17 @@ const names = [
   ];
   names.splice(1,1)
   console.log(names); 
+
+//   When will we be there
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  };
+  const travelTimeArrival= (travelInformation) =>{
+    let time = travelInformation.destinationDistance / travelInformation.speed;
+    let hour = Math.floor(time);
+    let minute = Math.floor((time % 1) *60);
+    return `${hour} hours and ${minute} minutes`
+  }
+  const travelTime = travelTimeArrival(travelInformation);
+  console.log(travelTime);
