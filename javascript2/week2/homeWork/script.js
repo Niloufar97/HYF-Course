4bd0a23963b4566195ff2d033c0b9e65b5888368
Niloufar <nilooooofar97@gmail.com>
2023-12-11 11:32:37 +0100

    short movies title array created

--- javascript2/week2/homeWork/script.js
+++ javascript2/week2/homeWork/script.js
@@ -1,1 +1,9 @@
-console.log(movies);
+const shortMovieTitles = movies.map(movie => {
+    const title  = movie.title;
+    const shortTitle = title.split(' ');
+    if(!shortTitle[1]){
+        shortTitle[1] = '';
+    }
+    return (shortTitle[0] + shortTitle[1])   
+})
+console.log(shortMovieTitles)
