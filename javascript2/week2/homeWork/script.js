5ee23a698cb2a6e9b836a552ad7802ddf1793edb
Niloufar <nilooooofar97@gmail.com>
2023-12-11 11:38:19 +0100

    movies long title and movies between 1980-1989

--- javascript2/week2/homeWork/script.js
+++ javascript2/week2/homeWork/script.js
@@ -6,4 +6,13 @@ const shortMovieTitles = movies.map(movie => {
     }
     return (shortTitle[0] + shortTitle[1])   
 })
-console.log(shortMovieTitles)
+// console.log(shortMovieTitles);
+const longMoviesTitle = movies.map(movie => movie.title);
+// console.log(longMoviesTitle);
+let countMovies = 0;
+movies.map(movie => {
+    if(movie.year >= 1980 && movie.year <= 1989){
+        countMovies++;
+    }
+})
+// console.log(countMovies);
