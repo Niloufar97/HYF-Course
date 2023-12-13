bfd763653b36592451a0d7a73b3aa3cc62e3bde7
Niloufar <nilooooofar97@gmail.com>
2023-12-13 09:29:10 +0100

    rendering products

--- javascript2/hyf-bay/main.js
+++ javascript2/hyf-bay/main.js
@@ -1,4 +1,22 @@
-console.log("Script loaded");
-
 const products = getAvailableProducts();
-console.log(products);
+const productsListUl = document.getElementById('product-list')
+function renderProducts(products) {
+        const renderedProducts = products.map((product) => {
+        const productUl = document.createElement('ul')
+        const listItemTitle = document.createElement('li');
+        listItemTitle.innerHTML= `<h2>${product.name}</h2>`;
+        const listItemPrice = document.createElement('li');
+        listItemPrice.textContent = product.price;
+        const listItemRating = document.createElement('li');
+        listItemRating.textContent = product.rating;
+
+        productUl.appendChild(listItemTitle);
+        productUl.appendChild(listItemPrice);
+        productUl.appendChild(listItemRating);
+        return productUl;
+    })
+    renderedProducts.forEach((productUl) => {
+        productsListUl.appendChild(productUl);
+    });
+}
+renderProducts(products)
